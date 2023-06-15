ba8b7a1e812ec80861e676483b30dd901d85a7f4
Jack Gee <jack.gee@protonmail.com>
2023-06-15 12:01:33 +0100

    Memoize functions with more than one argument

--- Memoizer.js
+++ Memoizer.js
@@ -1,21 +1,23 @@
 "use strict";
 function memoize(func) {
     const cache = new Map();
-    return (arg) => {
-        if (cache.has(arg)) {
-            return cache.get(arg);
+    const memoizedFn = (...args) => {
+        const cacheKey = JSON.stringify(args);
+        if (cache.has(cacheKey)) {
+            return cache.get(cacheKey);
         }
-        const result = func(arg);
-        cache.set(arg, result);
+        const result = func(...args);
+        cache.set(cacheKey, result);
         return result;
     };
+    return memoizedFn;
 }
-function expensiveCalculation(n) {
-    console.log("Performing expensive calculation...");
-    return n * 2;
+function sum(...numbers) {
+    console.log("Performing expensive sum...");
+    return numbers.reduce((acc, curr) => acc + curr, 0);
 }
-const memoizedCalculation = memoize(expensiveCalculation);
-console.log(memoizedCalculation(5)); // Performs expensive calculation... (returns 10)
-console.log(memoizedCalculation(5)); // (returns 10, retrieved from cache)
-console.log(memoizedCalculation(10)); // Performs expensive calculation... (returns 20)
-console.log(memoizedCalculation(10)); // (returns 20, retrieved from cache)
+const memoizedSum = memoize(sum);
+console.log(memoizedSum(1, 2)); // Performs expensive calculation... (returns 3)
+console.log(memoizedSum(1, 2)); // (returns 3, retrieved from cache)
+console.log(memoizedSum(1, 2, 3)); // Performs expensive calculation... (returns 6)
+console.log(memoizedSum(1, 2, 3)); // (returns 6, retrieved from cache)
--- src/Memoizer.ts
+++ src/Memoizer.ts
@@ -1,26 +1,29 @@
-type Func<T, R> = (arg: T) => R;
+type MemoizeCacheKey = string | number | symbol;
 
-function memoize<T, R>(func: Func<T, R>): Func<T, R> {
-    const cache: Map<T, R> = new Map();
+function memoize<T extends (...args: any[]) => any>(func: T): T {
+    const cache = new Map<MemoizeCacheKey, ReturnType<T>>();
 
-    return (arg: T): R => {
-        if (cache.has(arg)) {
-            return cache.get(arg)!;
+    const memoizedFn = (...args: Parameters<T>): ReturnType<T> => {
+        const cacheKey = JSON.stringify(args);
+        if (cache.has(cacheKey)) {
+            return cache.get(cacheKey)!;
         }
-        const result = func(arg);
-        cache.set(arg, result);
+        const result = func(...args);
+        cache.set(cacheKey, result);
         return result;
     };
+
+    return memoizedFn as T;
 }
 
-function expensiveCalculation(n: number): number {
-    console.log("Performing expensive calculation...");
-    return n * 2;
+function sum(...numbers: number[]): number {
+    console.log("Performing expensive sum...");
+    return numbers.reduce((acc, curr) => acc + curr, 0);
 }
 
-const memoizedCalculation = memoize(expensiveCalculation);
+const memoizedSum = memoize(sum);
 
-console.log(memoizedCalculation(5));  // Performs expensive calculation... (returns 10)
-console.log(memoizedCalculation(5));  // (returns 10, retrieved from cache)
-console.log(memoizedCalculation(10)); // Performs expensive calculation... (returns 20)
-console.log(memoizedCalculation(10)); // (returns 20, retrieved from cache)
+console.log(memoizedSum(1, 2));  // Performs expensive calculation... (returns 3)
+console.log(memoizedSum(1, 2));  // (returns 3, retrieved from cache)
+console.log(memoizedSum(1, 2, 3)); // Performs expensive calculation... (returns 6)
+console.log(memoizedSum(1, 2, 3)); // (returns 6, retrieved from cache)
